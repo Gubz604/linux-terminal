7590b6da166a4a1ef984b909433fbd4689658ec4
Gurvir <GurvirSD@Gmail.com>
2026-09-03 03:25:35 -0700

    Change Directory cd command implemented using the chdir system.

--- Shell/main.c
+++ Shell/main.c
@@ -6,11 +6,16 @@
 #include <string.h>
 
 int main() {
-
+    char cwd[1024];
     char user_input[1024];
 
     while (1) {
-        printf("$ ");
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            printf("%s $ ", cwd);
+        } else {
+            perror("getcwd");
+        }
+
         if (fgets(user_input, sizeof(user_input), stdin) == NULL) {
             break;
         } 
@@ -21,6 +26,29 @@ int main() {
             break;
         }
 
+        char *args[64];
+
+        int i = 0;
+
+        char *token = strtok(user_input, " \t\n");
+
+        while (token != NULL) {
+            args[i] = token;
+            i++;
+
+            token = strtok(NULL, " \t\n");
+        }
+
+        args[i] = NULL;
+
+        if (strcmp(args[0], "cd") == 0) {
+            if (chdir(args[1]) == -1) {
+                perror("chdir");
+            }
+
+            continue;
+        }
+
         pid_t pid = fork();
 
         if (pid < 0) {
@@ -28,21 +56,6 @@ int main() {
             exit(1);
         } else if (pid == 0) {
             // Child Process
-
-            char *args[64];
-
-            int i = 0;
-
-            char *token = strtok(user_input, " \t\n");
-
-            while (token != NULL) {
-                args[i] = token;
-                i++;
-
-                token = strtok(NULL, " \t\n");
-            }
-
-            args[i] = NULL;
 
             execvp(args[0], args);
             exit(1);
